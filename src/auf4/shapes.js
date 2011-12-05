236c0daa6ff50b8101d121a9aad4ad8e90037458
Rico P <MailToRico@googlemail.com>
2011-12-05 17:55:56 +0100

    Icasohedron

--- src/auf4/shapes.js
+++ src/auf4/shapes.js
@@ -31,7 +31,12 @@ VertexBasedShape = function(gl, primitiveType, numVertices) {
     
     // remember how many vertices this shape has
     this.numVertices = numVertices;
-    
+
+    // check if numVertices is a integer 
+    if(numVertices != (~~numVertices)) {
+        console.log("numVertices is not a full number."); 
+    }
+
     // add a vertex attribute to the shape
     this.addVertexAttribute = function(gl, attrType, dataType, 
                                         numElements,dataArray) {
@@ -186,43 +191,82 @@ Cube = function(gl, size) {
 */ 
 
 Sphere = function(gl, size) {
+	var vertices = getSphereVertices(); 
+	var indices = getSphereIndices();
+    var colors = getSphereVerticesColors();  
+
+	var vposition = makeFlat(vertices, indices); 
+
     // instantiate the shape as a member variable
-    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, 36);
+    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, vposition.length / 3);
 
     var s = size || 1.0; 
 
-    var vposition = new Float32Array( [ 
-        -s,-s, s,  s, s, s, -s, s, s, // a
-        -s,-s, s,  s,-s, s,  s, s, s, // b 
-         s,-s, s,  s,-s,-s,  s, s, s, // c 
-         s,-s,-s,  s, s,-s,  s, s, s, // d 
-         s, s, s,  s, s,-s, -s, s,-s, // e 
-        -s, s,-s, -s, s, s,  s, s, s, // f 
-        -s, s, s, -s,-s, s, -s,-s,-s, // g 
-        -s, s, s, -s,-s,-s, -s, s,-s, // h 
-        -s, s,-s, -s,-s,-s,  s,-s,-s, // i
-         s,-s,-s,  s, s,-s, -s, s,-s, // j
-        -s,-s,-s, -s,-s, s,  s,-s, s, // k
-         s,-s, s,  s,-s,-s, -s,-s,-s // l 
-    ]);
-
 	var a = 1, b = 0, c = 0.5;                          
 
-    var vcolor    = new Float32Array( [ 
-        a,a,a, a,a,a, a,a,a, 
-        a,a,b, a,a,b, a,a,b, 
-        a,b,a, a,b,a, a,b,a, 
-        a,b,b, a,b,b, a,b,b, 
-        b,a,a, b,a,a, b,a,a, 
-        b,a,b, b,a,b, b,a,b, 
-        b,b,a, b,b,a, b,b,a, 
-        a,a,c, a,a,c, a,a,c, 
-        a,c,a, a,c,a, a,c,a, 
-        a,c,c, a,c,c, a,c,c, 
-        c,a,a, c,a,a, c,a,a, 
-        c,a,c, c,a,c, c,a,c 
-    ]);
+    var vcolor = makeFlat(colors, indices); 
+    
     this.shape.addVertexAttribute(gl, "vertexPosition", gl.FLOAT, 3, vposition);
     this.shape.addVertexAttribute(gl, "vertexColor",    gl.FLOAT, 3, vcolor);
+}
+
+function getSphereVerticesColors() {
+	var X = 1; 
+    var Y = 0.2
+	var Z = 0.5;
+    var A = 0.8; 
+    var B = 0.7; 
+     
+	var v = [
+        [A, 1.0, Z], [X, 1.0, Z], [A, 1.0, B], [X, 1.0, B],
+        [1.0, Z, X], [1.0, Z, A], [1.0, B, X], [1.0, B, A],
+        [Z, X, 1.0], [B, X, 1.0], [Z, A, 1.0], [B, A, 1.0]
+    ]
+
+    return v; 
 }        
+
+function getSphereVertices() {
+    var X = 0.525731112119133606;
+    var Z = 0.850650808352039932;
+
+	var v = [
+        [-X, 0.0, Z], [X, 0.0, Z], [-X, 0.0, -Z], [X, 0.0, -Z],
+        [0.0, Z, X], [0.0, Z, -X], [0.0, -Z, X], [0.0, -Z, -X],
+        [Z, X, 0.0], [-Z, X, 0.0], [Z, -X, 0.0], [-Z, -X, 0.0]
+    ]
+
+    return v; 
+}
     
+function getSphereIndices() {
+    var i = [
+        [0,4,1],  [0,9,4],  [9,5,4],  [4,5,8],  [4,8,1],
+        [8,10,1], [8,3,10], [5,3,8],  [5,2,3],  [2,7,3],
+        [7,10,3], [7,6,10], [7,11,6], [11,0,6], [0,1,6],
+        [6,1,10], [9,0,11], [9,11,2], [9,2,5],  [7,2,11]
+    ];
+
+    return i; 
+}
+
+function makeFlat(vertices, indeces) {
+    var list = [];
+    for(var i = 0; i != indeces.length; i++) {
+        var indece = indeces[i]; 
+ 
+        pushTriangle( list, vertices[indece[0]][0], vertices[indece[0]][1], vertices[indece[0]][2] );  
+        pushTriangle( list, vertices[indece[1]][0], vertices[indece[1]][1], vertices[indece[1]][2] );  
+        pushTriangle( list, vertices[indece[2]][0], vertices[indece[2]][1], vertices[indece[2]][2] );  
+    }
+
+    return new Float32Array(list); 
+}
+
+function pushTriangle(list, v1, v2, v3) {
+    list.push(v1, v2, v3); 
+}
+
+
+
+
